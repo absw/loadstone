a1b375fe10240ab6ad085b4e2663c095754e9832
PabloMansanet <pablomansanet@gmail.com>
2020-05-15 12:30:05 +0200

    Add native unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-#![no_std]
-#![no_main]
+#![cfg_attr(test, allow(unused_imports))]
+#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(test), no_main)]
 
+#[cfg(not(test))]
 extern crate panic_semihosting; // logs messages to the host stderr
 
 use cortex_m_rt::entry;
 use stm32f4::stm32f429;
 use cortex_m_semihosting::hprintln;
 
+#[cfg(not(test))]
 #[entry]
 fn main() -> ! {
     let peripherals = stm32f429::Peripherals::take().unwrap();
@@ -22,4 +25,15 @@ fn main() -> ! {
         hprintln!("Turning LED off").unwrap();
         peripherals.GPIOB.bsrr.write(|w| w.br7().bit(true));
     }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn foo() {
+    println!("tests work!");
+    assert!(2 == 3);
+  }
 }
