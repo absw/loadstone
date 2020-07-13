afe274a332faac3d870c39bbf54cb37516fe3ed2
PabloMansanet <pablomansanet@gmail.com>
2020-07-13 09:45:31 +0200

    Refactor driver ports

--- src/lib.rs
+++ src/lib.rs
@@ -19,17 +19,27 @@ pub use stm32f4::stm32f429 as stm32pac;
 #[cfg(feature = "stm32f469")]
 #[doc(hidden)]
 pub use stm32f4::stm32f469 as stm32pac;
-
 #[cfg(target_arch = "arm")]
 extern crate panic_abort;
-
 #[macro_use]
 extern crate derive_is_enum_variant;
-
 extern crate static_assertions;
 
+// Define and export a specific port module (transparently pull
+// its namespace to the current one)
+macro_rules! port {
+    ($x:ident) => {
+        #[macro_use]
+        pub mod $x;
+        pub use self::$x::*;
+    }
+}
+
 #[macro_use]
-pub mod drivers;
+pub mod drivers {
+    #[cfg(any(feature = "stm32f407", feature = "stm32f412", feature = "stm32f429", feature = "stm32f469"))]
+    port!(stm32f4);
+}
 
 /// Hardware Abstraction Layer, containing interfaces
 /// for low level drivers.
