dd453b8f716abf8f9a5ce960ec2c758b44148cd8
PabloMansanet <pablomansanet@gmail.com>
2020-07-16 12:45:03 +0200

    Add complex read-write cycle to the POST sequence

--- src/devices/bootloader.rs
+++ src/devices/bootloader.rs
@@ -35,29 +35,54 @@ where
     pub fn power_on_self_test(&mut self) {
         let Self { external_flash, mcu_flash, post_led, serial } = self;
         let _guard = Guard::new(post_led, Toggle::on, Toggle::off);
-        Self::post_test_flash(external_flash).report_unwrap("[External Flash] ", serial);
-        uprintln!(serial, "External flash ID verification and RWR cycle passed");
-        Self::post_test_flash(mcu_flash).report_unwrap("[MCU Flash] ", serial);
-        uprintln!(serial, "MCU flash ID verification and RWR cycle passed");
+        Self::post_test_flash_simple_rwc(external_flash).report_unwrap("[External Flash] ", serial);
+        uprintln!(serial, "External flash ID verification and simple RW cycle passed");
+        Self::post_test_flash_simple_rwc(mcu_flash).report_unwrap("[MCU Flash] ", serial);
+        uprintln!(serial, "MCU flash ID verification and simple RW cycle passed");
+        Self::post_test_flash_complex_rwc(external_flash).report_unwrap("[External Flash] ", serial);
+        uprintln!(serial, "External flash complex RW cycle passed");
+        Self::post_test_flash_complex_rwc(mcu_flash).report_unwrap("[MCU Flash] ", serial);
+        uprintln!(serial, "MCU flash complex RW cycle passed");
     }
 
     pub fn run(self) -> ! { loop {} }
 
-    fn post_test_flash<F>(flash: &mut F) -> Result<(), Error>
+    fn post_test_flash_simple_rwc<F>(flash: &mut F) -> Result<(), Error>
     where
         F: flash::ReadWrite,
     {
-        let post_failed = Error::PostError("Flash Read Write cycle failed");
+        let failure = Error::PostError("Flash Read Write cycle failed");
         let mut magic_number_buffer = [0u8; 1];
         let mut new_magic_number_buffer = [0u8; 1];
         let (write_start, _) = F::writable_range();
         let (read_start, _) = F::readable_range();
-        block!(flash.read(read_start, &mut magic_number_buffer)).map_err(|_| post_failed)?;
+        block!(flash.read(read_start, &mut magic_number_buffer)).map_err(|_| failure)?;
         new_magic_number_buffer[0] = magic_number_buffer[0].wrapping_add(1);
-        block!(flash.write(write_start, &mut new_magic_number_buffer)).map_err(|_| post_failed)?;
-        block!(flash.read(read_start, &mut magic_number_buffer)).map_err(|_| post_failed)?;
+        block!(flash.write(write_start, &mut new_magic_number_buffer)).map_err(|_| failure)?;
+        block!(flash.read(read_start, &mut magic_number_buffer)).map_err(|_| failure)?;
         if magic_number_buffer != new_magic_number_buffer {
-            Err(Error::PostError("Flash Read Write cycle failed"))
+            Err(failure)
+        } else {
+            Ok(())
+        }
+    }
+
+    fn post_test_flash_complex_rwc<F>(flash: &mut F) -> Result<(), Error>
+    where
+        F: flash::ReadWrite,
+    {
+        let failure = Error::PostError("Complex Flash Read Write cycle failed");
+        let magic_word_buffer = [0xAAu8, 0xBBu8, 0xCCu8, 0xDDu8];
+        let superset_byte_buffer = [0xFFu8];
+        let expected_final_buffer = [0xFFu8, 0xBBu8, 0xCCu8, 0xDDu8];
+        let (write_start, _) = F::writable_range();
+        let (read_start, _) = F::readable_range();
+        block!(flash.write(write_start, &magic_word_buffer)).map_err(|_| failure)?;
+        block!(flash.write(write_start, &superset_byte_buffer)).map_err(|_| failure)?;
+        let mut final_buffer = [0x00; 4];
+        block!(flash.read(read_start, &mut final_buffer)).map_err(|_| failure)?;
+        if expected_final_buffer != final_buffer {
+            Err(failure)
         } else {
             Ok(())
         }
