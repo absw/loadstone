26256c3dc8d6afc7d47c9380d27ddbbc3ff1f991
PabloMansanet <pablomansanet@gmail.com>
2020-07-17 11:12:19 +0200

    WIP CLI

--- src/devices/cli.rs
+++ src/devices/cli.rs
@@ -15,6 +15,7 @@ pub enum Error {
     CommandEmpty,
     CommandUnknown,
     MalformedArguments,
+    CharactersNotAllowed,
 }
 
 pub struct Cli { }
@@ -27,12 +28,17 @@ enum Argument<'a> {
     Pair(&'a str, &'a str),
 }
 
-const FIELD_SEPARATOR: char = ' ';
 const ARGUMENT_SEPARATOR: char = '=';
+const ALLOWED_TOKENS: &str = " =_";
 
 impl Cli {
     fn parse<'a>(text: &'a str) -> Result<(Name, impl Iterator<Item=Argument<'a>>), Error> {
-        let mut tokens = text.trim_end_matches(" \r\n").split(FIELD_SEPARATOR);
+        let text = text.trim_end_matches(|c:char| c == '\r' || c == '\n' || c.is_whitespace());
+        if !text.chars().all(|c| c.is_alphanumeric() || ALLOWED_TOKENS.contains(c)) {
+            return Err(Error::CharactersNotAllowed)
+        }
+
+        let mut tokens = text.split_whitespace();
         let name = tokens.next().ok_or(Error::CommandEmpty)?;
         if !tokens.clone().all(|t| t.split(ARGUMENT_SEPARATOR).count() <= 2) {
             return Err(Error::MalformedArguments);
@@ -63,9 +69,27 @@ mod test {
     fn basic_command_parsing() {
         let sample_command = "my_command an_option=5000 some_flag";
         let (name, mut arguments) = Cli::parse(sample_command).unwrap();
-
         assert_eq!("my_command", name);
         assert_eq!(Argument::Pair("an_option","5000"), arguments.next().unwrap());
         assert_eq!(Argument::Single("some_flag"), arguments.next().unwrap());
+
+        let sample_command = "command         with_too_much_whitespace   but  still=valid   \r\n\r\n";
+        let (name, mut arguments) = Cli::parse(sample_command).unwrap();
+        assert_eq!("command", name);
+        assert_eq!(Argument::Single("with_too_much_whitespace"), arguments.next().unwrap());
+        assert_eq!(Argument::Single("but"), arguments.next().unwrap());
+        assert_eq!(Argument::Pair("still", "valid"), arguments.next().unwrap());
+    }
+
+    #[test]
+    fn parsing_fails_for_various_bad_commands() {
+        let bad_command_no_fields = "";
+        assert_eq!(Error::CommandEmpty, Cli::parse(bad_command_no_fields).err().unwrap());
+
+        let bad_command_strange_formatting = "command with=a=strange=argument";
+        assert_eq!(Error::MalformedArguments, Cli::parse(bad_command_strange_formatting).err().unwrap());
+
+        let bad_command_characters_not_allowed = "com-mand with? bad+characters";
+        assert_eq!(Error::CharactersNotAllowed, Cli::parse(bad_command_characters_not_allowed).err().unwrap());
     }
 }
