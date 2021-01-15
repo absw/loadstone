1e11c6dc427b3064809c5656eb7674522a37a02d
Arron Speake <arron.speake@bluefruit.co.uk>
2021-01-15 11:25:27 +0000

    Add basic clap interface.

--- tools/signing_tool/src/main.rs
+++ tools/signing_tool/src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::App::new("Loadstone Image Signing Tool")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(clap::Arg::with_name("image")
+            .index(1)
+            .required(true)
+            .help("The firmware image to be signed."))
+        .arg(clap::Arg::with_name("private_key")
+            .index(2)
+            .required(true)
+            .help("The private key used to sign the image."))
+        .get_matches();
+
+    println!("{:?}", matches);
 }
