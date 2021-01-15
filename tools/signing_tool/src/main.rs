99dccdfeb8515c0d0050c4add9cc9fe732aa3995
Arron Speake <arron.speake@bluefruit.co.uk>
2021-01-15 15:36:52 +0000

    Refactor clap App creation.

--- tools/signing_tool/src/main.rs
+++ tools/signing_tool/src/main.rs
@@ -5,6 +5,8 @@ mod signing;
 use crate::signing::*;
 
 extern crate clap;
+use clap::{clap_app};
+
 extern crate base64;
 
 use std::{
@@ -68,19 +70,13 @@ fn run_with_file_names(image: String, key: String) -> Result<String, String> {
 }
 
 fn main() {
-    let matches = clap::App::new("Loadstone Image Signing Tool")
-        .version(env!("CARGO_PKG_VERSION"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(clap::Arg::with_name("image")
-            .index(1)
-            .required(true)
-            .help("The firmware image to be signed."))
-        .arg(clap::Arg::with_name("private_key")
-            .index(2)
-            .required(true)
-            .help("The private key used to sign the image."))
-        .get_matches();
+    let matches = clap_app!(myapp =>
+        (version: env!("CARGO_PKG_VERSION"))
+        (author: env!("CARGO_PKG_AUTHORS"))
+        (about: env!("CARGO_PKG_DESCRIPTION"))
+        (@arg image: +required "The firmware image to be signed.")
+        (@arg private_key: +required "The private key used to sign the image.")
+    ).get_matches();
 
     let image = matches.value_of("image")
         .unwrap()
