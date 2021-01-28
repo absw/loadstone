bbde9afdd12d2b320f941a1d6298ccfce384cdb2
PabloMansanet <pablomansanet@gmail.com>
2021-01-28 16:20:38 +0100

    Improve message for corner case

--- src/devices/bootloader.rs
+++ src/devices/bootloader.rs
@@ -227,6 +227,10 @@ where
             input_image.size()
         );
         if must_be_golden && !input_image.is_golden() {
+            duprintln!(
+                self.serial,
+                "Image is not golden.",
+            );
             return Err(Error::DeviceError("Image is not golden"));
         }
         let input_image_start_address = input_bank.location;
