9715866ff71492cb96ac4251c675fdee3ff86d4a
Arron Speake <arron.speake@bluefruit.co.uk>
2021-02-17 13:54:21 +0000

    Add elm support for upload tab.

--- tools/webserver/web_assets/src/Main.elm
+++ tools/webserver/web_assets/src/Main.elm
@@ -4,9 +4,9 @@ import Browser exposing (Document, document)
 import Html exposing (..)
 import Html.Attributes exposing (class, id, value, src)
 import Html.Events exposing (onClick)
-import Browser.Navigation
 import Http exposing (expectJson)
 import Json.Decode as Decode
+import File exposing (File)
 
 type alias Flags = ()
 
@@ -14,24 +14,25 @@ type alias Metrics = { time: String, path: String }
 
 type alias MetricsInfo = { error: String, time: String, path: String }
 
-type Model = WaitingForInfo
-    | Error String
-    | Info Metrics
+type Tab = Info | Upload
+
+type InfoTab = Waiting | InfoDone (Result String Metrics)
+
+type UploadTab = NotStarted | FileSelected File | InProgress () | UploadDone (Result String ())
+
+type alias Model = { tab: Tab, info: InfoTab, upload: UploadTab }
 
 type Message = RecievedInfo (Result Http.Error MetricsInfo)
     | MainButtonClick
 
--- init --
+-- utility --
 
-init : Flags -> (Model, Cmd Message)
-init _ =
-    (
-        WaitingForInfo,
-        Http.get {
-            url = "/api/metrics",
-            expect = expectJson RecievedInfo decode_json_metrics
-        }
-    )
+make_metrics_request : Cmd Message
+make_metrics_request = Http.get
+    {
+        url = "/api/metrics",
+        expect = expectJson RecievedInfo decode_json_metrics
+    }
 
 decode_json_metrics : Decode.Decoder MetricsInfo
 decode_json_metrics =
@@ -39,6 +40,19 @@ decode_json_metrics =
         (Decode.field "error" Decode.string)
         (Decode.field "time" Decode.string)
         (Decode.field "path" Decode.string)
+
+-- init --
+
+init : Flags -> (Model, Cmd Message)
+init _ = (get_default_model, make_metrics_request)
+
+get_default_model : Model
+get_default_model =
+    {
+        tab = Info,
+        info = Waiting,
+        upload = NotStarted
+    }
 
 -- view --
 
@@ -65,17 +79,36 @@ view_body model =
 
 get_model_id : Model -> String
 get_model_id model =
-    case model of
-        WaitingForInfo -> "loading"
-        Info _ -> "info"
-        Error _ -> "error"
+    case model.tab of
+        Info -> get_info_model_id model.info
+        Upload -> get_upload_model_id model.upload
 
 get_model_main : Model -> List (Html Message)
 get_model_main model =
-    case model of
-        WaitingForInfo -> view_loading "Waiting for metrics from remote server..."
-        Info metrics -> view_info metrics
-        Error message -> view_error message
+    case model.tab of
+        Info -> get_info_main model.info
+        Upload -> get_upload_main model.upload
+
+get_info_model_id : InfoTab -> String
+get_info_model_id info =
+    case info of
+        Waiting -> "loading"
+        InfoDone (Err _) -> "error"
+        InfoDone (Ok _) -> "info"
+
+get_upload_model_id : UploadTab -> String
+get_upload_model_id _ = "loading"
+
+get_info_main : InfoTab -> List (Html Message)
+get_info_main info =
+    case info of
+        Waiting -> view_loading "Waiting for metrics from remote server..."
+        InfoDone (Ok metrics) -> view_info metrics
+        InfoDone (Err error) -> view_error error
+
+get_upload_main : UploadTab -> List (Html Message)
+get_upload_main _ =
+    view_loading "This page hasn't been written yet..."
 
 view_loading : String -> List (Html Message)
 view_loading message =
@@ -118,39 +151,29 @@ view_info_pane name description value =
 -- update --
 
 update : Message -> Model -> (Model, Cmd Message)
-update message model =
-    case model of
-        WaitingForInfo -> update_waiting_for_info message
-        Info _ -> (model, Cmd.none)
-        Error error -> update_error error message
+update message model = (model, Cmd.none)
+    |> update_info message
+    |> update_upload message
 
-update_waiting_for_info : Message -> (Model, Cmd Message)
-update_waiting_for_info message =
+update_info : Message -> (Model, Cmd Message) -> (Model, Cmd Message)
+update_info message (model, command) =
     case message of
-        RecievedInfo (Err error) -> (Error (get_http_error_message error), Cmd.none)
-        RecievedInfo (Ok info) -> (handle_recieved_info info, Cmd.none)
-        _ -> (WaitingForInfo, Cmd.none)
+        RecievedInfo info -> ({ model | info = handle_recieved_info info }, command)
+        MainButtonClick -> ({ model | info = Waiting }, make_metrics_request)
 
-update_error : String -> Message -> (Model, Cmd Message)
-update_error error message =
-    case message of
-       MainButtonClick -> (Error error, Browser.Navigation.reload)
-       _ -> (Error error, Cmd.none)
+update_upload : Message -> (Model, Cmd Message) -> (Model, Cmd Message)
+update_upload _ model_command = model_command
 
-get_http_error_message : Http.Error -> String
-get_http_error_message error = 
-    case error of
-        Http.Timeout -> "The network timed out."
-        Http.NetworkError -> "An unknown network error occured."
-        Http.BadStatus status -> "HTTP error " ++ (String.fromInt status) ++ " occured."
-        _ -> "An internal error occured."
-
-handle_recieved_info : MetricsInfo -> Model
-handle_recieved_info info =
-    if info.error == "none" then
-        Info { time = info.time, path = info.path }
-    else
-        Error (get_info_error_message info.error)
+handle_recieved_info : Result Http.Error MetricsInfo -> InfoTab
+handle_recieved_info result =
+    case result of
+        Ok info ->
+            if info.error == "none" then
+                InfoDone (Ok { time = info.time, path = info.path })
+            else
+                InfoDone (Err (get_info_error_message info.error))
+        Err error ->
+            InfoDone (Err (get_http_error_message error))
 
 get_info_error_message : String -> String
 get_info_error_message error =
@@ -160,6 +183,14 @@ get_info_error_message error =
        "io" -> "The server initialised a connection to the device, but failed to communicate."
        "metrics" -> "The server recieved malformed metrics information from the device."
        _ -> "An unknown error occured when recieving the metrics."
+
+get_http_error_message : Http.Error -> String
+get_http_error_message error = 
+    case error of
+        Http.Timeout -> "The network timed out."
+        Http.NetworkError -> "An unknown network error occured."
+        Http.BadStatus status -> "HTTP error " ++ (String.fromInt status) ++ " occured."
+        _ -> "An internal error occured."
 
 -- subscriptions --
 
