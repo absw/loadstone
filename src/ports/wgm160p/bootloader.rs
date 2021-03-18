f17d34a783317faa58857d5f4239f653937ab247
pablo-mansanet-bluefruit <pablo.mansanet@bluefruit.co.uk>
2021-03-18 12:44:18 +0100

    Fix WGM memory map

--- src/ports/wgm160p/bootloader.rs
+++ src/ports/wgm160p/bootloader.rs
@@ -3,24 +3,19 @@
 use blue_hal::{drivers::efm32gg11b::flash::{self, Flash}, efm32pac, hal::null::{NullFlash, NullSerial, NullSystick}};
 
 use crate::{devices::{bootloader::Bootloader, image}, error::{self, Error}};
+use blue_hal::KB;
 
-const NUMBER_OF_BANKS: usize = 3;
-const IMAGE_SIZE: usize = {
-    let (start, end) = (flash::Address(0), flash::Address(0 + flash::Map::size() as u32));
-    let total_size = (end.0 - start.0) as usize;
-    total_size / NUMBER_OF_BANKS
-};
+const IMAGES_START: flash::Address = flash::Address(KB!(64));
+const IMAGE_SIZE: usize = KB!(660);
 
 const fn image_offset(index: usize) -> flash::Address {
-    flash::Address((index * IMAGE_SIZE) as u32)
+    flash::Address(IMAGES_START.0 + (index * IMAGE_SIZE) as u32)
 }
-
-pub static MCU_BANKS: [image::Bank<flash::Address>; NUMBER_OF_BANKS] = [
+pub static MCU_BANKS: [image::Bank<flash::Address>; 3] = [
     image::Bank { index: 1, bootable: true, location: image_offset(0), size: IMAGE_SIZE, is_golden: false },
     image::Bank { index: 2, bootable: false, location: image_offset(1), size: IMAGE_SIZE, is_golden: false },
     image::Bank { index: 3, bootable: false, location: image_offset(2), size: IMAGE_SIZE, is_golden: true },
 ];
-
 impl Bootloader<NullFlash, Flash, NullSerial, NullSystick> {
     pub fn new() -> Self {
         let peripherals = efm32pac::Peripherals::take().unwrap();
