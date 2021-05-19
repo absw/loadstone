fc407c834bec3b78bf172a6ffd144b4dbc987055
Arron Speake <arron.speake@bluefruit.co.uk>
2021-05-19 11:20:24 +0100

    Add target argument to build_artifacts.sh.

--- build_artifacts.sh
+++ build_artifacts.sh
@@ -3,20 +3,31 @@
 # exit on any failure
 set -e
 
+# Setup
 mkdir -p artifacts/
 
+# Argument handling
+if [[ $# == 0 ]]; then
+    echo "Missing target argument."
+    exit 1
+fi
+
+function generate_artifact {
+    cargo objcopy --bin $1 --features $2 --release --target thumbv7em-none-eabihf -- -O binary $3
+}
+
 # Package self-booting artifacts
-cargo gen_loadstone
-cargo gen_demo_app
+generate_artifact loadstone $1 loadstone.bin
+generate_artifact demo_app $1 demo_app.bin
 mv loadstone.bin artifacts/loadstone.bin
 mv demo_app.bin artifacts/demo_app_self_booting.bin
 
 # Package bootable artifacts
-LOADSTONE_USE_ALT_MEMORY=1 cargo gen_demo_app
+LOADSTONE_USE_ALT_MEMORY=1 generate_artifact demo_app $1 demo_app.bin
 cp demo_app.bin artifacts/demo_app_unsigned.bin
 cp demo_app.bin artifacts/demo_app_golden.bin
 cp demo_app.bin artifacts/demo_app_regular.bin
-LOADSTONE_USE_ALT_MEMORY=1 cargo gen_variant_demo_app
+LOADSTONE_USE_ALT_MEMORY=1 generate_artifact demo_app_variant $1 demo_app_variant.bin
 cp demo_app_variant.bin artifacts/
 
 pushd tools/signing_tool/ >/dev/null
