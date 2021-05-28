967cb5f8b5fa23bba45330a2a2b7285a56a8288f
pablo-mansanet-bluefruit <pablo.mansanet@bluefruit.co.uk>
2021-05-28 10:49:31 +0200

    Move docs folder to crate root

--- loadstone_front/build_web.sh
+++ loadstone_front/build_web.sh
@@ -13,7 +13,7 @@ CRATE_NAME_SNAKE_CASE="${CRATE_NAME//-/_}" # for those who name crates with-keba
 export RUSTFLAGS=--cfg=web_sys_unstable_apis
 
 # Clear output from old stuff:
-rm -f docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm
+rm -f ../docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm
 
 echo "Building rust…"
 BUILD=release
@@ -22,13 +22,13 @@ cargo build --release -p ${CRATE_NAME} --lib --target wasm32-unknown-unknown
 echo "Generating JS bindings for wasm…"
 TARGET_NAME="${CRATE_NAME_SNAKE_CASE}.wasm"
 wasm-bindgen "target/wasm32-unknown-unknown/${BUILD}/${TARGET_NAME}" \
-  --out-dir docs --no-modules --no-typescript
+  --out-dir ../docs --no-modules --no-typescript
 
 # to get wasm-opt:  apt/brew/dnf install binaryen
 # echo "Optimizing wasm…"
 # wasm-opt docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm -O2 --fast-math -o docs/${CRATE_NAME_SNAKE_CASE}_bg.wasm # add -g to get debug symbols
 
-echo "Finished: docs/${CRATE_NAME_SNAKE_CASE}.wasm"
+echo "Finished: ../docs/${CRATE_NAME_SNAKE_CASE}.wasm"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # Linux, ex: Fedora
--- loadstone_front/start_server.sh
+++ loadstone_front/start_server.sh
@@ -8,5 +8,5 @@ cargo install basic-http-server
 
 echo "open http://localhost:8080"
 
-(cd docs && basic-http-server --addr 127.0.0.1:8080 .)
-# (cd docs && python3 -m http.server 8080 --bind 127.0.0.1)
+(cd ../docs && basic-http-server --addr 127.0.0.1:8080 .)
+# (cd ../docs && python3 -m http.server 8080 --bind 127.0.0.1)
