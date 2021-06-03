c32e23d77cc9f1df2796319e678cf9add4a41abe
pablo-mansanet-bluefruit <pablo.mansanet@bluefruit.co.uk>
2021-06-03 10:38:16 +0200

    WIP override ports/pin_configuration

--- src/ports/mod.rs
+++ src/ports/mod.rs
@@ -6,7 +6,7 @@
 use blue_hal::port;
 
 #[cfg(feature = "stm32f412_discovery")]
-port!(stm32f412_discovery: [bootloader, pin_configuration, boot_manager,]);
+port!(stm32f412_discovery: [bootloader, boot_manager,]);
 
 #[cfg(feature = "wgm160p")]
 port!(wgm160p: [bootloader,]);
