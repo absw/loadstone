aa1a596d620ee2a9783b00f1865a12304e112bd0
Arron Speake <arron.speake@bluefruit.co.uk>
2021-07-13 10:51:05 +0100

    Refactor main code.

--- tools/config-generator/src/main.rs
+++ tools/config-generator/src/main.rs
@@ -1,41 +1,56 @@
 use std::{borrow::Cow, io::{Read, Write}};
 use loadstone_config::{Configuration, features::Greetings};
 
-fn main() {
+fn read_input_string() -> Result<String, String> {
     let mut input = String::default();
-    if let Err(e) = std::io::stdin().read_to_string(&mut input) {
-        eprintln!("Error: failed to read from standard input stream: {}.", e);
-        return;
+    let result = std::io::stdin().read_to_string(&mut input);
+    match result {
+        Ok(_) => Ok(input),
+        Err(e) => Err(format!("failed to read from standard input stream: {}.", e)),
     }
+}
 
-    let mut configuration = if input == "" {
-        Configuration::default()
+fn get_input_configuration(string: String) -> Result<Configuration, String> {
+    if string == "" {
+        Ok(Default::default())
     } else {
-        match ron::from_str(&input) {
-            Ok(o) => o,
-            Err(e) => {
-                eprintln!("Error: failed to load configuration from input: {}", e);
-                return;
-            }
-        }
-    };
+        ron::from_str(&string)
+            .map_err(|e| format!("failed to load configuration from input: {}.", e))
+    }
+}
 
+fn modify_configuration(mut configuration: Configuration) -> Configuration {
     // TODO: Use command line arguments to modify configuration.
     configuration.feature_configuration.greetings = Greetings::Custom {
         loadstone: Cow::from("Hello"),
         demo: Cow::from("Goodbye"),
     };
+    configuration
+}
 
-    let output = match ron::to_string(&configuration) {
-        Ok(o) => o,
-        Err(e) => {
-            eprintln!("Error: failed to write configuration to output: {}", e);
-            return;
-        }
-    };
+fn get_output_string(configuration: Configuration) -> Result<String, String> {
+    ron::to_string(&configuration)
+        .map_err(|e| format!("failed to write configuration to output: {}.", e))
+}
 
-    if let Err(e) = std::io::stdout().write_all(output.as_bytes()) {
-        eprintln!("Error: failed to write output to standard output stream: {}", e);
-        return;
+fn write_output_string(string: String) -> Result<(), String> {
+    std::io::stdout().write_all(string.as_bytes())
+        .map_err(|e| format!("failed to write output to standard output stream: {}.", e))
+}
+
+fn run() -> Result<(), String> {
+    let input = read_input_string()?;
+    let configuration = get_input_configuration(input)?;
+    let new_configuration = modify_configuration(configuration);
+    let output = get_output_string(new_configuration)?;
+    write_output_string(output)
+}
+
+fn main() {
+    let result = run();
+
+    if let Err(e) = result {
+        eprintln!("Error: {}", e);
+        std::process::exit(1)
     }
 }
